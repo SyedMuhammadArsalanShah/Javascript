dc12e92a46a94191dbed2b92cc9b5bd0465b8b6c
SyedMuhammadArsalanShah <smarsalanshah125@gmail.com>
2022-05-09 16:55:16 +0500

    lec01 in javascript

---
+++ Lecture01LogsAndError.js
@@ -0,0 +1,23 @@
+document.write("hassan khalid");
+
+console.time("code timing ");
+console.log("Abdulhannan");
+console.log(24+6);
+console.log(24>45);
+console.log(["affan","zaynab","hasnain","Azaad chaiwala"]);
+console.log({faris:'student of the year', marks:34,age:16.5});
+console.table({faris:'student of the year', marks:34,age:16.5});
+console.warn("its a warning");
+
+
+
+console.assert(222 >180, "age >180 this is not possible");
+
+console.error("this an error");
+console.timeEnd("code timing ");
+// console.clear();
+
+
+
+
+
